bb1e3bc20cf922972d54c06e7b169ff606045381
David Eads <davideads@gmail.com>
2019-12-02 15:53:58 -0600

    strip enumerated columns because hasura doesn't like them

--- sql/tables/CanElections.sql
+++ sql/tables/CanElections.sql
@@ -1,22 +1,3 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'candidacy_race_type') THEN
-      CREATE TYPE candidacy_race_type AS ENUM (
-          'incumbent',
-          'challenger',
-          'open seat',
-          'retired'
-      );
-    END IF;
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'candidacy_outcome') THEN
-        CREATE TYPE candidacy_outcome AS ENUM (
-            'won',
-            'lost'
-        );
-    END IF;
-END$$;
-
-
 CREATE UNLOGGED TABLE IF NOT EXISTS raw.canelections (
     id integer primary key,
     candidateid integer,
