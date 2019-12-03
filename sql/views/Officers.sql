95936f8f7a13410ae3c222c5953fae98bd6cda3e
David Eads <davideads@gmail.com>
2019-12-02 18:04:24 -0600

    finalize making all derive views consistent
    it was kind of a mishmash of il sunshine style and the isboe's style

--- sql/views/Officers.sql
+++ sql/views/Officers.sql
@@ -1,4 +1,17 @@
 CREATE TABLE public.officers AS
-    SELECT * FROM raw.officers
+    SELECT
+        id as id,
+        lastname as last_name,
+        firstname as first_name,
+        address1 as address1,
+        address2 as address2,
+        city as city,
+        state as state,
+        zip as zip,
+        title as title,
+        phone as phone,
+        redactionrequested as redaction_requested
+    FROM
+        raw.officers
 ;
 ALTER TABLE public.officers ADD PRIMARY KEY (id);
